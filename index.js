f26b447112bf4f9b154afa016c93da91fa534c64
Chris Atkin <atkinchris@gmail.com>
2019-03-12 18:11:52 +0000

    Begin adding auth endpoints

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
 const port = 3000
 
+app.post('/-/v1/login', (req, res) => {
+  if (!(req.body && req.body.username)) {
+    res.sendStatus(401)
+    return
+  }
+
+  res.sendStatus(200)
+})
+
+app.put('/-/user/:user', (req, res) => {
+  const { name, password } = req.body
+
+  if (name === password) {
+    res.status(201).json({ token: 'atkinchris--token' })
+  } else {
+    res.status(401).json({ error: 'invalid credentials' })
+  }
+})
+
 app.all('*', (req, res) => {
-  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-  console.log(req.method, url)
+  console.log(req.method, req.originalUrl, req.body, req.headers)
   res.send('Hello World!')
 })
 
