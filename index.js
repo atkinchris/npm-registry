2fae54457a27df772a87ac03e1329f1a41d705ca
Chris Atkin <atkinchris@gmail.com>
2019-03-12 21:00:42 +0000

    Add JWT generation

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressJwt = require('express-jwt')
+const jwt = require('jsonwebtoken')
+
+const config = {
+  secret: 'chris',
+}
 
 const app = express()
-
 app.use(bodyParser.json())
 const port = 3000
+const withToken = expressJwt({ secret: config.secret })
 
-app.post('/-/v1/login', (req, res) => {
-  if (!(req.body && req.body.username)) {
-    res.sendStatus(401)
-    return
-  }
-
-  res.sendStatus(200)
-})
+app.post('/-/v1/login', (_, res) => res.sendStatus(401))
 
 app.put('/-/user/:user', (req, res) => {
   const { name, password } = req.body
 
-  if (name === password) {
-    res.status(201).json({ token: 'atkinchris--token' })
-  } else {
-    res.status(401).json({ error: 'invalid credentials' })
+  if (name !== password) {
+    res.status(401).json({ error: 'Invalid username or password' })
+    return
   }
+
+  const token = jwt.sign({ username: name }, config.secret, { noTimestamp: true })
+  res.status(201).json({ token })
 })
 
+app.get('/-/whoami', withToken, (req, res) => res.json(req.user))
+
 app.all('*', (req, res) => {
+  console.log('Unhandled request')
   console.log(req.method, req.originalUrl, req.body, req.headers)
-  res.send('Hello World!')
+  res.sendStatus(501)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
