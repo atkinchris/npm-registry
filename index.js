f0ac68fc31bddd7830156f181214589ef31a1e72
Chris Atkin <atkinchris@gmail.com>
2019-03-12 14:04:08 +0000

    Log on all paths and verbs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.all('*', (req, res) => {
   const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
   console.log(url)
   res.send('Hello World!')
