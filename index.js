4829586a2c453e976df554b40eebed753b1bce34
Chris Atkin <atkinchris@gmail.com>
2019-03-12 21:27:52 +0000

    Proxy requests to upstream

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressJwt = require('express-jwt')
 const jwt = require('jsonwebtoken')
+const proxy = require('express-http-proxy')
 
 const config = {
   secret: 'chris',
+  upstream: 'https://registry.npmjs.org/',
 }
 
 const app = express()
 app.use(bodyParser.json())
 const port = 3000
 const withToken = expressJwt({ secret: config.secret })
+const proxyUpstream = proxy(config.upstream)
 
 app.post('/-/v1/login', (_, res) => res.sendStatus(401))
 
@@ -27,6 +30,14 @@ app.put('/-/user/:user', (req, res) => {
 })
 
 app.get('/-/whoami', withToken, (req, res) => res.json(req.user))
+
+app.post('/-/npm/v1/security/audits', (_, res) => res.sendStatus(501))
+app.post('/-/npm/v1/security/audits*', (_, res) => res.sendStatus(501))
+
+app.get('/:package', (req, res, next) => {
+  console.log('Getting package')
+  return proxyUpstream(req, res, next)
+})
 
 app.all('*', (req, res) => {
   console.log('Unhandled request')
