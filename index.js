5f39ee5f30467ce76be25d64f132bf6035bd4e99
Chris Atkin <atkinchris@gmail.com>
2019-03-12 13:59:17 +0000

    Add basic Express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+  console.log(url)
+  res.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
