b867719afcee0233b152b24eac65a58f50b559b8
Chris Atkin <atkinchris@gmail.com>
2019-03-13 14:14:14 +0000

    Build package definiton for package family

--- src/routes/package.js
+++ src/routes/package.js
@@ -17,8 +17,9 @@ router.get('/:package', (req, res, next) => {
 
 router.put('/:package', withToken, async (req, res) => {
   const pkg = req.body
+  const { _attachments: attachments, name, versions, 'dist-tags': distTags } = pkg
 
-  const tags = Object.keys(pkg['dist-tags'])
+  const tags = Object.keys(distTags)
   const tag = tags[0]
 
   if (tags.length !== 1 || !tag) {
@@ -26,7 +27,8 @@ router.put('/:package', withToken, async (req, res) => {
     return
   }
 
-  const { _attachments: attachments, name } = pkg
+  const storageFolder = 'storage'
+  const pkgFolder = `${storageFolder}/${name}`
 
   // eslint-disable-next-line no-restricted-syntax
   for (const [filename, attachment] of Object.entries(attachments)) {
@@ -37,17 +39,33 @@ router.put('/:package', withToken, async (req, res) => {
     const sha = hash.digest('hex')
     const ext = path.extname(filename)
     const baseName = path.basename(filename, ext)
-    const tarballPath = `storage/${name}/${baseName}`
-    const tarballName = `${sha}${ext}`
+    const tarballFolder = path.join(pkgFolder, baseName)
+    const tarballPath = path.join(tarballFolder, `${sha}${ext}`)
 
-    await fs.mkdirp(tarballPath)
-    await fs.writeFile(path.join(tarballPath, tarballName), data, {
+    await fs.mkdirp(tarballFolder)
+    await fs.writeFile(tarballPath, data, {
       'Content-Type': attachment.content_type,
       'Content-Length': attachment.length,
     })
   }
 
-  res.status(200).json(pkg)
+  const pkgDefinition = {
+    name,
+    versions: {
+      ...versions,
+    },
+    'dist-tags': {
+      ...distTags,
+    },
+  }
+  const pkgPath = path.join(pkgFolder, 'manifest.json')
+  await fs.writeFile(pkgPath, JSON.stringify(pkgDefinition, null, '\t'))
+
+  const registry = [name]
+  const registryPath = path.join(storageFolder, 'registry.json')
+  await fs.writeFile(registryPath, JSON.stringify(registry, null, '\t'))
+
+  res.status(200).json(pkgDefinition)
 })
 
 module.exports = router
